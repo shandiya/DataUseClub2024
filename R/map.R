efffb8b5c27d213fe3e0def9aff3402dad2f7a7a
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-02-26 16:20:30 +1100

    adds place names to map

--- R/map.R
+++ R/map.R
@@ -5,13 +5,19 @@ library(ozmaps)
 
 bbox <- as.vector(st_bbox(ozmap_country))
 
-m <- leaflet() |> 
-  #setView(lng = 134.2, lat = -25.3, zoom = 4) |> 
+data <- data.frame(
+  latitude = c(-34.9, -33.8, -37.8),
+  longitude = c(138.6, 151.2, 144.9),
+  place = c("Tarntanya | Adelaide", "Warrane | Sydney", "Naarm | Melbourne"),
+  people = c("Kaurna", "Gadigal, Eora Nation", "Wurundjeri, Kulin Nation")
+)
+
+m <- leaflet(data) |> 
   addProviderTiles(providers$CartoDB.VoyagerNoLabels) |> 
   fitBounds(bbox[1], bbox[2], bbox[3], bbox[4]) |> 
-  addCircleMarkers(lng = c(138.6, 151.2, 144.9), 
-                   lat = c(-34.9, -33.8, -37.8), 
-                   popup = c("Kaurna", "Gadigal, Eora Nation", "Wurundjeri, Kulin Nation"),
+  addCircleMarkers(~longitude,
+                   ~latitude,
+                   popup = paste0("<strong>", data$place, "</strong><br>", "<i>", data$people, "<br><i>"),
                    color = "#273b50", 
                    stroke = FALSE, 
                    fillOpacity = 0.7,

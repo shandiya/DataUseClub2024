f175f63a8d659b5b206358b362e8d7a147ba94be
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-02-26 14:58:45 +1100

    more wip slides + adds code blocks to technical section

--- R/functions.R
+++ R/functions.R
@@ -6,14 +6,8 @@ gt_theme <- function(data, ...){
         default_fonts())) |> 
     opt_row_striping() |> 
     tab_options(
-      table.font.size = 39,
-      #table.font.weight = "lighter",
-      column_labels.font.size = 50,
-      #column_labels.font.weight = "lighter",
       row.striping.background_color = "#fffbf0",
       table_body.hlines.color = "transparent",
-      table.font.color = "#273b50",
-      data_row.padding = 12,
       column_labels.background.color = "#273b50",
       ...
     ) 
